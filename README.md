39cb2e0efeb372a23a66bbd15e78ce1f1350acf2
David Veza <veza.david@gmail.com>
2023-11-30 15:23:20 +0200

    Add markdown preview

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a neovim config heavily inspired from ThePrimeagen's 0 to lsp video.
+
+And after that adapted for Python development.
+
+External dependencies:
+- ripgrep (for telescope live grep)
+- npm (for mason package management)
--- lua/david/packer.lua
+++ lua/david/packer.lua
@@ -38,6 +38,8 @@ return require('packer').startup(function(use)
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
 		}
-	}
+    }
+
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
 end)
