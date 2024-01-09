71eaa5e61ab615a64757df8de714346ed0063022
David Veza <david.veza@ascentcore.com>
2024-01-09 09:22:39 +0200

    Add tmux nav plugin

--- lua/david/packer.lua
+++ lua/david/packer.lua
@@ -42,4 +42,20 @@ return require('packer').startup(function(use)
 
     use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
+
+    use { 'alexghergh/nvim-tmux-navigation', config = function()
+        require'nvim-tmux-navigation'.setup {
+            disable_when_zoomed = true, -- defaults to false
+            keybindings = {
+                left = "<C-h>",
+                down = "<C-j>",
+                up = "<C-k>",
+                right = "<C-l>",
+                last_active = "<C-\\>",
+                next = "<C-Space>",
+            }
+        }
+    end
+}
+
 end)
